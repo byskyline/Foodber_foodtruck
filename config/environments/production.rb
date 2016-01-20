1eee4192b7832727e46c15245415f43bc71e0297
ellis <byskyline2158@gmail.com>
2016-01-20 20:20:21 +0800

    config.serve_static_files = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.serve_static_assets = true
+  config.serve_static_files = true
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
