015236549739438150cac1361720faa17bb007b1
ellis <byskyline2158@gmail.com>
2016-01-25 15:57:34 +0800

    master login

--- app/controllers/food_trucks_controller.rb
+++ app/controllers/food_trucks_controller.rb
@@ -5,14 +5,14 @@ class FoodTrucksController < ApplicationController
   # GET /food_trucks.json
   def index
     if current_user
-      if current_user.email == 'wei1208@livemail.tw'
+      if current_user.email == 'wei1208@livemail.tw' || 'milemaple2158@yahoo.com.tw'
       @food_trucks = FoodTruck.page(params[:page]).per(3)
       elsif @food_trucks = FoodTruck.where( email: current_user.email ).page(params[:page]).per(3)
       else @food_trucks = nil
       end
     end
 
-      
+
   end
 
   # GET /food_trucks/1
